1c81a474f59ba31d4c28753960676dae450c39aa
79096 <a.malakhinsky@gmail.com>
2024-03-06 15:25:08 +0300

    checking and editing

--- utlis_/functions.py
+++ utlis_/functions.py
@@ -1,14 +1,14 @@
 import datetime
 import json
 
-from class_Print_history import Print_history
 
 
 def import_json():
     """ Открывает json объект и возвращает его"""
-    with open('operations.json', 'r', encoding='utf-8') as file:
-        return json.load(file)
+    # with open('operations.json', 'r', encoding='utf-8') as file:
+    with open('C:\\Users\\79096\\PycharmProjectsGit\\CW_3\\CW_3\\utlis_\\operations.json', 'r', encoding='utf-8') as file:
 
+        return json.load(file)
 
 def check_error(word):
     """ Проверка на наличие ключа в словаре, возвращает False
@@ -20,13 +20,13 @@ def check_error(word):
         return True
 
 
-def print_sorted_list(min_range=0, max_range=5):
+def print_sorted_list(Print_history_, min_range=0, max_range=5):
     """ Сортировка по убыванию дат, возварщает отсортированный список индексов,
     который используеться для создания экземпляров по убыванию дат """
 
     list_sort = list()
     for i in range(min_range, max_range):
-        copy_print_history = Print_history(import_json(), i)
+        copy_print_history = Print_history_(import_json(), i)
         d = (copy_print_history.date_print())
         list_sort.append(d)
     sorted_values = sorted(list_sort, key=lambda x: datetime.datetime.strptime(x, "%d.%m.%Y"), reverse=True)
