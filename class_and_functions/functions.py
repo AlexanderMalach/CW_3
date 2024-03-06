c4ce6c217f3b080edc6c03a41b44fa0ed6906c73
79096 <a.malakhinsky@gmail.com>
2024-03-06 10:37:32 +0300

    create fuctions: check_error()

--- class_and_functions/functions.py
+++ class_and_functions/functions.py
@@ -4,3 +4,11 @@ import json
 def import_json():
     with open('operations.json', 'r', encoding='utf-8') as file:
         return json.load(file)
+
+
+def check_error(word):
+    if word == None:
+        print("Что-то не так с ключом :(\nКакое-то значение будет \'None\' ")
+        return False
+    else:
+        return True
