5096a1d83860e6138a992cb5bf1f3c3bcd80b349
79096 <a.malakhinsky@gmail.com>
2024-03-06 12:23:52 +0300

    main

---
+++ main.py
@@ -0,0 +1,10 @@
+from class_and_functions.functions import import_json
+from class_and_functions.Class_Print_history import Print_history
+from class_and_functions.functions import print_sorted_list
+
+sort_list = print_sorted_list()
+list_dict = import_json()
+for i in sort_list:
+    copy = Print_history(list_dict(), i)
+    print((
+              f'\n{copy.date_print()} {copy.print_line('description')}\n{copy.account_code('from')} ->{copy.account_code('to')}\n{copy.currency()}\n'))
