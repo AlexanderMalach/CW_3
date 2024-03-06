e62991b50d1d1b158aaa40bdc4aaf4ba09c853d6
79096 <a.malakhinsky@gmail.com>
2024-03-06 10:43:33 +0300

    add docstring: import_json() and check_error()

--- class_and_functions/functions.py
+++ class_and_functions/functions.py
@@ -2,11 +2,14 @@ import json
 
 
 def import_json():
+    """ Открывает json объект и возвращает его"""
     with open('operations.json', 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
 def check_error(word):
+    """ Проверка на наличие ключа в словаре, возвращает False
+или True """
     if word == None:
         print("Что-то не так с ключом :(\nКакое-то значение будет \'None\' ")
         return False
